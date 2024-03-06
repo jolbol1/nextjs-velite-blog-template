3282885582ae844eafa60e7e2991fa8a03d8a982
jolbol1 <github@jamesshopland.com>
2024-03-06 17:24:41 +0000

    Stage 10: Pagination

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,25 @@
 import { posts } from "#site/content";
 import { PostItem } from "@/components/post-item";
+import { QueryPagination } from "@/components/query-pagination";
 import { sortPosts } from "@/lib/utils";
 
-export default async function BlogPage() {
+const POSTS_PER_PAGE = 5;
+
+interface BlogPageProps {
+  searchParams: {
+    page?: string;
+  };
+}
+
+export default async function BlogPage({ searchParams }: BlogPageProps) {
+  const currentPage = Number(searchParams?.page) || 1;
   const sortedPosts = sortPosts(posts.filter((post) => post.published));
-  const displayPosts = sortedPosts;
+  const totalPages = Math.ceil(sortedPosts.length / POSTS_PER_PAGE);
+
+  const displayPosts = sortedPosts.slice(
+    POSTS_PER_PAGE * (currentPage - 1),
+    POSTS_PER_PAGE * currentPage
+  );
 
   return (
     <div className="container max-w-4xl py-6 lg:py-10">
@@ -36,6 +51,7 @@ export default async function BlogPage() {
       ) : (
         <p>Nothing to see here yet</p>
       )}
+      <QueryPagination totalPages={totalPages} className="justify-end mt-4" />
     </div>
   );
 }
---
+++ components/query-pagination.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { usePathname, useSearchParams } from "next/navigation";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationPrevious,
+  PaginationNext,
+} from "./ui/pagination";
+
+interface QueryPaginationProps {
+  totalPages: number;
+  className?: string;
+}
+
+export function QueryPagination({
+  totalPages,
+  className,
+}: QueryPaginationProps) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const currentPage = Number(searchParams.get("page")) || 1;
+
+  const prevPage = currentPage - 1;
+  const nextPage = currentPage + 1;
+
+  const createPageURL = (pageNumber: number | string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", pageNumber.toString());
+    return `${pathname}?${params.toString()}`;
+  };
+
+  return (
+    <Pagination className={className}>
+      <PaginationContent>
+        {prevPage >= 1 ? (
+          <PaginationItem>
+            <PaginationPrevious href={createPageURL(prevPage)} />
+          </PaginationItem>
+        ) : null}
+
+        {Array(totalPages)
+          .fill("")
+          .map((_, index) => (
+            <PaginationItem
+              className="hidden sm:inline-block"
+              key={`page-button-${index}`}
+            >
+              <PaginationLink
+                isActive={currentPage === index + 1}
+                href={createPageURL(index + 1)}
+              >
+                {index + 1}
+              </PaginationLink>
+            </PaginationItem>
+          ))}
+
+        {nextPage <= totalPages ? (
+          <PaginationItem>
+            <PaginationNext href={createPageURL(nextPage)} />
+          </PaginationItem>
+        ) : null}
+      </PaginationContent>
+    </Pagination>
+  );
+}
