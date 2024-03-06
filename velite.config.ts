9dc51fa83949a99fb292e2cb1ae65b5e2af1b1ea
jolbol1 <github@jamesshopland.com>
2024-03-06 14:37:46 +0000

    Stage 2: setup velite

---
+++ velite.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig, defineCollection, s } from "velite";
+
+const computedFields = <T extends { slug: string }>(data: T) => ({
+  ...data,
+  slugAsParams: data.slug.split("/").slice(1).join("/"),
+});
+
+const posts = defineCollection({
+  name: "Post",
+  pattern: "blog/**/*.mdx",
+  schema: s
+    .object({
+      slug: s.path(),
+      title: s.string().max(99),
+      description: s.string().max(999).optional(),
+      date: s.isodate(),
+      published: s.boolean().default(true),
+      body: s.mdx(),
+    })
+    .transform(computedFields),
+});
+
+export default defineConfig({
+  root: "content",
+  output: {
+    data: ".velite",
+    assets: "public/static",
+    base: "/static/",
+    name: "[name]-[hash:6].[ext]",
+    clean: true,
+  },
+  collections: { posts },
+  mdx: {
+    rehypePlugins: [],
+    remarkPlugins: [],
+  },
+});
