28ef2b1afaf7831a327abe0b7ec7a424cca34026
jolbol1 <github@jamesshopland.com>
2024-03-06 18:20:18 +0000

    Stage 16: Fix z index header

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -9,7 +9,7 @@ import { ModeToggle } from "./mode-toggle";
 
 export function SiteHeader() {
   return (
-    <header className="sticky top-0 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="z-10 sticky top-0 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <MainNav />
         <div className="flex flex-1 items-center justify-end space-x-2">
