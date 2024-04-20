0868cadcb5ec9428e5465f19c2965a25e794d534
hunterho07 <hunterho.my@gmail.com>
2024-04-20 14:21:46 +0800

    changed the name to hunter

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <section className="space-y-6 pb-8 pt-6 md:pb-12 md:mt-10 lg:py-32">
         <div className="container flex flex-col gap-4 text-center">
           <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-balance">
-            Hello, I&apos;m James
+            Hello, I&apos;m Hunter
           </h1>
           <p className="max-w-[42rem] mx-auto text-muted-foreground sm:text-xl text-balance">
             Welcome to my blog template. Built using tailwind, shadcn, velite
